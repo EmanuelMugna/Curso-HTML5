997fc815e0489f5d9ad968bf479ec63a1815837e
Emanuel <emanuelmugna@gmail.com>
2024-09-10 15:59:13 -0300

    Identidicacion de elementos en Java

--- js/cod.js
+++ js/cod.js
@@ -1,5 +1,15 @@
 /* Mi primer programa Java*/
 
+function inicio(){
+
+for(var i=0;i<3;i++){
+document.getElementsByTagName("p")[i ].onclick=ejecuta;
+   
+} 
+}
+
 function ejecuta(){
     alert('Mensaje on Click');
 }
+
+window.onload=inicio;
