587db57599d1f8648fb8a7b4d28db95c7ff15380
Emanuel <emanuelmugna@gmail.com>
2024-09-12 11:29:49 -0300

    Multiples eventos sobre un mismo elemento

--- js/cod.js
+++ js/cod.js
@@ -1,15 +1,31 @@
 /* Mi primer programa Java*/
 
+/*Declaracion de variables*/
+
+var foto;
+
+////////////////////////////////////
+
 function inicio(){
 
- var foto = document.getElementsByTagName("img")[ 0];
- foto.addEventListener("click", ejecuta,false)
-
-
+ foto = document.getElementsByTagName("img")[ 0];
+ foto.addEventListener("mouseover", crecer,false)
+ foto.addEventListener("mouseout", decrecer,false)
+ foto.addEventListener("click",ejecuta,false)
 }
 
 function ejecuta(){
     alert('Mensaje on Click');
+}
+
+function crecer(){
+    foto.width=225;
+    foto.heigth=225;
+}
+
+function decrecer(){
+    foto.width=200;
+    foto.heigth=200;
 }
 
 window.onload=inicio;
