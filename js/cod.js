198a39588374de1a18659418e319118719fce61a
Emanuel <emanuelmugna@gmail.com>
2024-09-16 14:22:21 -0300

    formulario con validacion de campos

--- js/cod.js
+++ js/cod.js
@@ -1,64 +1,46 @@
-// /* Mi primer programa Java*/
-
-// /*Declaracion de variables*/
-
-// var foto;
-
-// ////////////////////////////////////
-
-// function inicio(){
-
-//  foto = document.getElementsByTagName("img")[ 0];
-//  foto.addEventListener("click",ejecuta,false)
- 
-//  parrafo = document.getElementById("parrafo");
-//  parrafo.addEventListener("click",ejecuta2,true);
-//  // parrafo.addEventListener("click",ejecuta2,false);  // doy prioridad a segundo cartel (debo siempre leer secuencialmente)
-// }
-
-// function ejecuta(){
-//     alert('Mensaje on Click de imagen');
-// }
-
-// function ejecuta2(){
-//     alert('Mensaje on Click de parrafo');
-// }
-
-// validacion de formulario
-
 
 
 function comenzar(){
 
-var nombre=document.getElementById("nombre");
-
-var apellido=document.getElementById("apellido");
-
-nombre.addEventListener("input",validar, false);
-
-apellido.addEventListener("input",validar, false);
-
-validar();
-
-}
-
-function validar(){
-
-    if (nombre.value=="" && apellido.value==""){
-
-        nombre.setCustomValidity("Introduce nombre o apellido");
-        nombre.style.background="#ffdddd";
-        apellido.style.background="#ffdddd";
+    edad=document.getElementById("edad");
+    edad.addEventListener("change",cambia_rango,false);
+    
+    document.registro_usuario.addEventListener("invalid", validar,true);
+    document.getElementById("enviar").addEventListener("click",enviar,false);
+    
     }
-
-    else{
-
-        nombre.setCustomValidity(""); //importante poner mayusula
-        nombre.style.background="#ffffff";
-        apellido.style.background="#ffffff";
+    
+    function cambia_rango (){
+    
+        var salida=document.getElementById("rango");
+        var calculo=edad.value-30;
+    
+        if (calculo<30){
+            calculo=0;
+    
+            edad.value=30;
+        }
+    
+        salida.innerHTML=calculo+" a "+edad.value;
     }
-
-}
-
-window.addEventListener("load",comenzar,false);
-//window.onload=inicio;
+    
+    function validar (e){
+    
+        var elemento=e.target;
+        elemento.style.background="#ffdddd";
+    
+    }
+    
+    function enviar(){
+    
+        var correcto=document.registro_usuario.checkValidity();
+    
+        if(correcto==true){
+            document.registro_usuario.submit();
+        }
+    }
+    
+    
+    
+    window.addEventListener("load",comenzar,false);
+    
