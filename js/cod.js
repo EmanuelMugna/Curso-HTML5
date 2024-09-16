8e5b80e5bbd43f843144588fb69b65c83c94c22e
Emanuel <emanuelmugna@gmail.com>
2024-09-16 10:21:18 -0300

    verificacion de formulario

--- js/cod.js
+++ js/cod.js
@@ -1,28 +1,64 @@
-/* Mi primer programa Java*/
+// /* Mi primer programa Java*/
 
-/*Declaracion de variables*/
+// /*Declaracion de variables*/
 
-var foto;
+// var foto;
 
-////////////////////////////////////
+// ////////////////////////////////////
 
-function inicio(){
+// function inicio(){
 
- foto = document.getElementsByTagName("img")[ 0];
- foto.addEventListener("click",ejecuta,false)
+//  foto = document.getElementsByTagName("img")[ 0];
+//  foto.addEventListener("click",ejecuta,false)
  
- parrafo = document.getElementById("parrafo");
- parrafo.addEventListener("click",ejecuta2,true);
- // parrafo.addEventListener("click",ejecuta2,false);  // doy prioridad a segundo cartel (debo siempre leer secuencialmente)
+//  parrafo = document.getElementById("parrafo");
+//  parrafo.addEventListener("click",ejecuta2,true);
+//  // parrafo.addEventListener("click",ejecuta2,false);  // doy prioridad a segundo cartel (debo siempre leer secuencialmente)
+// }
+
+// function ejecuta(){
+//     alert('Mensaje on Click de imagen');
+// }
+
+// function ejecuta2(){
+//     alert('Mensaje on Click de parrafo');
+// }
+
+// validacion de formulario
+
+
+
+function comenzar(){
+
+var nombre=document.getElementById("nombre");
+
+var apellido=document.getElementById("apellido");
+
+nombre.addEventListener("input",validar, false);
+
+apellido.addEventListener("input",validar, false);
+
+validar();
+
 }
 
-function ejecuta(){
-    alert('Mensaje on Click de imagen');
+function validar(){
+
+    if (nombre.value=="" && apellido.value==""){
+
+        nombre.setCustomValidity("Introduce nombre o apellido");
+        nombre.style.background="#ffdddd";
+        apellido.style.background="#ffdddd";
+    }
+
+    else{
+
+        nombre.setCustomValidity(""); //importante poner mayusula
+        nombre.style.background="#ffffff";
+        apellido.style.background="#ffffff";
+    }
+
 }
 
-function ejecuta2(){
-    alert('Mensaje on Click de parrafo');
-}
-
-
-window.onload=inicio;
+window.addEventListener("load",comenzar,false);
+//window.onload=inicio;
